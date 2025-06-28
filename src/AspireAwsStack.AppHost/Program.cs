7ec15b11c36e9bb02684c18ce603b6fcf18ca315
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-06-28 18:59:08 +0530

    Add initial project configuration files for .NET 9.0 and package management

--- src/AspireAwsStack.AppHost/Program.cs
+++ src/AspireAwsStack.AppHost/Program.cs
@@ -1,5 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+//// Set up a configuration for the AWS SDK for .NET.
+//var awsConfig = builder.AddAWSSDKConfig()
+//                        .WithProfile("dev")
+//                        .WithRegion(RegionEndpoint.USWest2);
+
 var cache = builder.AddRedis("cache");
 
 var apiService = builder.AddProject<Projects.AspireAwsStack_ApiService>("apiservice");
